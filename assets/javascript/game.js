91bdfaad24400837c1f86ed861f00d4064c6fc52
MsMinion <wolfgirl2004@hotmail.com>
2017-08-23 20:20:45 -0400

    Added functionality so right number of underscores print under 'current word' section.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,55 @@ var wins = 0;
 var guessesRemaining = 10;
 var alreadyGuessed = [];
 var currentWord;
+var currentChosenPokemon = [];
+var currentChosenDisplay = [];
+var pokemonArray = ["pikachu", "meowth", "togepi", "houndoom", "mudkip", "skitty", "lucario", "combee", "stoutland", "krookodile", "litleo", "dedenne", "silvally", "stufful"];
 
-//Take in keyboard input and print to browser
-document.onkeyup = function hangmanGame (event) {
+
+//Computer generates new random pokemon at beginning of game 
+if(guessesRemaining == 10) {
+    
+    var chosenPokemon = pokemonArray[Math.floor(Math.random() * pokemonArray.length)]; 
+    currentChosenPokemon.splice(0, 1, chosenPokemon);
+    currentChosenPokemon.length;
+}
+
+//Prints underscores to the browser display equal to length of chosen pokemon
+
+if(guessesRemaining == 10) {
+    
+        for(var i = 0; i < currentChosenPokemon[0].length; i++) {
+            currentChosenDisplay.push("_");
+            var properDisplayWord = currentChosenDisplay.join(" ");
+            document.getElementById("current-word").innerText = properDisplayWord;        
+      }
+    
+    }
+
+console.log("The pokemon the computer chose: " + currentChosenPokemon + " " + "length: " + currentChosenPokemon[0].length);
+console.log("Should show blank spaces" + currentChosenDisplay + " " + "length: " + currentChosenDisplay.length);
+
+//START USER INPUT FUNCTION
+
+document.onkeyup = function (event) {
     var userChoice = event.key;
+
+//Prints the letters that the user already guessed to the browser display.
+
+    if(guessesRemaining > 0){
+    
     alreadyGuessed.push(" " + userChoice + " ");
     var properDisplay = alreadyGuessed.join(" ");
     document.getElementById("already-guessed").innerText = properDisplay;
-}
+    } 
 
-function wordChoice () {
-    var pokemon = ["pikachu", "meowth", "togepi", "houndoom", "mudkip", "skitty", ];
+    if(){
+        
+    }
 
-}
+
+} /*End Master Function*/
+
 
 //IF letter is part of alreadyGuessed, don't do anything. Set as parameter for first part.
 
